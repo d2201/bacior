a613a08a1d7650a1386c1eda64ce88876764c2d2
d2201 <torozniks+gh@gmail.com>
2023-01-07 05:25:33 +0100

    fix deploy_prod script

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -2,4 +2,4 @@ source $PWD/scripts/env_source.sh
 
 echo "Upgrading prod deployment in project $PROJECT_ID"
 
-helm upgrade --install -f ./config/prod.yaml --set image=europe-west3-docker.pkg.dev/$PROJECT_ID/bacior/app:$(git rev-parse master) charts ./charts
+helm upgrade --install -f ./config/prod.yaml --set image=europe-west3-docker.pkg.dev/$PROJECT_ID/bacior/app:$(git rev-parse master) app-charts ./app-charts
