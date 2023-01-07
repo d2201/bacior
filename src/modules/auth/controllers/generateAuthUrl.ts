66d4a9b23804f4a6db16ea58486de44358f41797
d2201 <torozniks+gh@gmail.com>
2023-01-07 07:10:38 +0100

    update auth scopes

--- src/modules/auth/controllers/generateAuthUrl.ts
+++ src/modules/auth/controllers/generateAuthUrl.ts
@@ -1,9 +1,10 @@
 import { ControllerFunction } from '../../../types'
 import * as services from '../services'
+import { SheetsScopeStrategy } from '../strategies'
 import { ProfileScopeStrategy } from '../strategies/scope/profileScopeStrategy'
 
 export const generateAuthUrl: ControllerFunction = async (req, res) => {
-  const url = await services.generateAuthUrl(new ProfileScopeStrategy())
+  const url = await services.generateAuthUrl(new ProfileScopeStrategy(), new SheetsScopeStrategy())
 
   res.redirect(url)
 }
